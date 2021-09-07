d67b1b5e53ffb5592617f1ceab3b1bf84c321ca2
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-09-07 14:23:55 +0300

    added a space

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -6,4 +6,4 @@ for dig1 in range(0, 9):
             if (dig1 == 8) and (dig2 == 9):
                 print("{}{}".format(dig1, dig2))
             else:
-                print("{}{}".format(dig1, dig2), end=",")
+                print("{}{}".format(dig1, dig2), end=", ")
