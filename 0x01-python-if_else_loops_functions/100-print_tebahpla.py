4fee4bdf4aafd4c9510d635d178ba8c84f24a787
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-09-07 17:20:20 +0300

    added whitespace

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for i in range(122, 96, -1):
     alt = True
     if i % 2 != 0:
         alt = False
-    print("{}".format(chr(i) if alt else chr(i - 32)),end='')
+    print("{}".format(chr(i) if alt else chr(i - 32)), end='')
