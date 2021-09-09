4cc7913bcafed15c9073348231f7387c43a06733
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-09-09 22:03:00 +0300

    obeyed pep8

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     import sys
     len = len(sys.argv) - 1
     print("{} argument{}{}"
-            .format(len, "" if len == 1 else "s", "." if len == 0 else ":"))
+          .format(len, "" if len == 1 else "s", "." if len == 0 else ":"))
     i = 1
     while i <= len:
         print("{}: {}".format(i, sys.argv[i]))
-        i+=1;
+        i += 1
