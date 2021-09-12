398eb758dbe418f7f7b5f3b63fa2955cad7bb829
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-09-12 20:31:16 +0300

    changed method

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    len = 0
-    for i in matrix:
-        len += 1
-    for i in matrix:
-        for x in i:
-            print("{}{}".format(x, " " if x != i[-1] else "\n"), end="")
-    if not matrix:
-        print("{}".format(""))
+    if not matrix: 
         return None
+    for submat in matrix:
+        print(" ".join("{:d}".format(elmt) for elmt in submat))
