b697b074e22ca6f038179b9b04508869b6c59aa6
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-09-12 18:53:20 +0300

    changed range

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     len = 0
     for i in my_list:
         len += 1
-    for x in range((len - 1), -1, -1):
+    for x in range(-1, -(len + 1), -1):
         print("{:d}".format(my_list[x]))
