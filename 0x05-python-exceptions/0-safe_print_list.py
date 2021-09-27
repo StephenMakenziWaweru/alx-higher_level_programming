1f1ebe77bf14e7dbd375d44b02f671e6dc70fc18
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-09-27 12:48:28 +0300

    used while

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
-        for i in range(x):
+        while i is not x:
             print(my_list[i], end="")
+            i += 1
     except IndexError:
-        print()
-        return i
+        None
     print()
-    return i + 1
+    return i
