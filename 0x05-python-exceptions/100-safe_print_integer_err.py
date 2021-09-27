1a55e367ab0629c4904def7dece3e2d33a6fc9c4
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-09-27 15:36:05 +0300

    used exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError as e:
+    except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
     return True
