07d164dc23235eb77dab0c8ef0e835da25d1a38c
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-09-27 15:00:28 +0300

    removed append method

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     i = 0
-    res = []
+    res = [0] * list_length
 
     while i < list_length:
         try:
-            res.append(my_list_1[i] / my_list_2[i])
+            res[i] = (my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
-            res.append(0)
             print("division by 0")
         except TypeError:
-            res.append(0)
             print("wrong type")
         except IndexError:
-            res.append(0)
             print("out of range")
 
         i += 1
