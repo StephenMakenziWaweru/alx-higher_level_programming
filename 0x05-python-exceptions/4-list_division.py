dbc4453d65684a5abc93dd56c7ae879b43bab793
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-09-27 15:04:09 +0300

    added finally

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,5 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
 
-        i += 1
+        finally:
+            i += 1
     return res
