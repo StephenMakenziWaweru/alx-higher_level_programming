cc56e8d6369a0f7f48cce0e56f4e73dfa45e88bf
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-09-28 14:58:42 +0300

    added module docstrings

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/py
+"""Square module - assigns size of the square and
+checks for type and value"""
+
+
 class Square:
     """This is an empty class"""
     pass
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Square module - assigns size of the square and
+checks for type and value"""
+
+
 class Square:
     """defines a square with private instance attribute size"""
     def __init__(self, size=0):
