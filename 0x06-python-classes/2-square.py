63c6602482d1777c92d632e5fc87013dda805a8b
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-09-28 13:19:10 +0300

    task 2'

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """defines a square with private instance attribute size"""
+    def __init__(self, size=0):
+        """assigns size of the square and checks for type and value"""
+        try:
+            self.__size = int(size)
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        except TypeError:
+            print("size must be an integer")
