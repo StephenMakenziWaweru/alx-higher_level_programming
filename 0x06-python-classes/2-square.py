926eb01774eb60c93fe6c30c9b7cfc4861843f90
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-09-28 15:10:00 +0300

    changed how we check type

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,9 +7,9 @@ class Square:
     """Defines a square with private instance attribute size"""
     def __init__(self, size=0):
         """assigns size of the square and checks for type and value"""
-        try:
-            self.__size = int(size)
-            if size < 0:
-                raise ValueError("size must be >= 0")
-        except TypeError:
-            print("size must be an integer")
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
