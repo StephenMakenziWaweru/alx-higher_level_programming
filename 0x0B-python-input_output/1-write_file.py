0def1c060b1119104b3164f0e6d163ca68b8cad5
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-10-12 14:41:23 +0300

    added doc

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,6 @@
 
 
 def write_file(filename="", text=""):
+    """that writes a string to a text file and returns no. of chars written"""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
