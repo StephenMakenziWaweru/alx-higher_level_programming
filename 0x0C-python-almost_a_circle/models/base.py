c71cc91883f01dcdaa8eaeb1ec366a10b1e4a9cb
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-10-15 10:49:08 +0300

    added to_json_str method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base class module"""
+import json
 
 
 class Base():
@@ -13,3 +14,11 @@ class Base():
         else:
             Base.___nb_objects += 1
             self.id = Base.___nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns JSON string rep of list_dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
