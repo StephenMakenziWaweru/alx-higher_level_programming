66be80fc71adb28e81ec2204d4efed4b8ae64b70
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-10-15 16:56:17 +0300

    csv save and load methods

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Base class module"""
 import json
+import csv
 
 
 class Base():
@@ -25,7 +26,7 @@ class Base():
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """writes json string rep of list_objs"""
+        """writes json string rep of list_objs to .json file"""
         if list_objs is not None:
             list_objs = [i.to_dictionary() for i in list_objs]
         filename = cls.__name__ + ".json"
@@ -51,10 +52,42 @@ class Base():
 
     @classmethod
     def load_from_file(cls):
-        """loads instance attributes from file and creates instances"""
+        """loads instance attributes from .json file and creates instances"""
         try:
             with open(cls.__name__ + ".json", "r", encoding="utf-8") as f:
                 return [cls.create(**i) for i in
                         cls.from_json_string(f.read())]
         except FileNotFoundError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """writes list_objs to csv file"""
+        if list_objs is not None:
+            if cls.__name__ == "Rectangle":
+                list_objs = [[o.id, o.width, o.height, o.x, o.y]
+                             for o in list_objs]
+            elif cls.__name__ == "Square":
+                list_objs = [[o.id, o.size, o.x, o.y] for o in list_objs]
+            with open(cls.__name__ + ".csv", "w", encoding="utf-8") as f:
+                writer = csv.writer(f)
+                writer.writerows(list_objs)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """loads instance attributes from csv file and creates instances"""
+        ret = []
+        try:
+            with open(cls.__name__ + ".csv", "r", encoding="utf-8") as f:
+                reader = csv.reader(f)
+                for obj in reader:
+                    v = [eval(i) for i in obj]
+                    if cls.__name__ == "Rectangle":
+                        dct = {"id": v[0], "width": v[1], "height": v[2],
+                               "x": v[3], "y": v[4]}
+                    elif cls.__name__ == "Square":
+                        dct = {"id": v[0], "size": v[1], "x": v[2], "y": v[3]}
+                    ret.append(cls.create(**dct))
+            return ret
+        except FileNotFoundError:
+            return ret
