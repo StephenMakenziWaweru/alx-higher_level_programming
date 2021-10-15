a0adecac7c9d5b82c058312578bd5c16dd53b8d6
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-10-15 11:27:53 +0300

    added json_string_to_file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,16 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns JSON string rep of list_dictionaries"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or not list_dictionaries:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes json string rep of list_objs"""
+        if list_objs is not None:
+            list_objs = [i.to_dictionary() for i in list_objs]
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_objs))
