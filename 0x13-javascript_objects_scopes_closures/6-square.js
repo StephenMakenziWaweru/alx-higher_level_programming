aaebfe81df3dda368578d725f0206d5a8e576276
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-12-14 20:13:48 +0300

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const Rectangle = require('./5-square');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let rec = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        if (c) {
+          rec += 'C';
+        } else {
+          rec += 'X';
+        }
+      }
+      if (i < this.height - 1) {
+        rec += '\n';
+      }
+    }
+    console.log(rec);
+  }
+}
+
+module.exports = Square;
