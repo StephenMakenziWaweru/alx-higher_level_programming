cb718fd35b5116b6d5380e400f42d35d34584023
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-09 15:27:06 +0300

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
-import urllib.request
+""" Fetches https://intranet.hbtn.io/status with urlib"""
+from urllib import request
 
-if __name__ == '__main__':
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        html = response.read()
-        print("Body response:\n\t- type: {}\n\t- content: {}\n\t-".format(
-              type(html), html), "utf8 content:", html.decode("utf-8"))
+with request.urlopen("https://intranet.hbtn.io/status") as response:
+    r = response.read()
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+          .format(type(r), r, r.decode('utf-8')))
