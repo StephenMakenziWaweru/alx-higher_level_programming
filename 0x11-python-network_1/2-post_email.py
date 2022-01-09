14f895af924380b7e9c74ba3ac2a5945fe50924c
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-09 11:32:48 +0300

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,9 +5,9 @@ from sys import argv
 
 
 if len(argv) > 2:
-    email = {'email' : argv[2]}
+    email = {'email': argv[2]}
     data = parse.urlencode(email).encode('ascii')
     req = request.Request(argv[1], data)
     with request.urlopen(req) as response:
         r = response.read()
-        print(r.decode('utf-8')
+        print(r.decode('utf-8'))
