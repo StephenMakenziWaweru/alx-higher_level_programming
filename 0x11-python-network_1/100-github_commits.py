0a02949ddf2f77fa715b2cd22e250225d4ca282c
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-09 15:22:40 +0300

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""list 10 commits (from the most recent to oldest) of the repository rails'
-by the user 'rails'"""
+""" Github code challenge"""
 import requests
 from sys import argv
 
-
-if __name__ == '__main__':
-    repo = argv[1]
-    owner = argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(repo, owner)
-
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits"\
+          .format(argv[2], argv[1])
     r = requests.get(url)
-    try:
-        r = r.json()
-        count = 0
-        for commit in r:
-            if count < 10:
-                sha = commit.get('sha')
-                author = commit.get('commit').get('author').get('name')
-                print('{}: {}'.format(sha, author))
-            count += 1
-    except ValueError:
-        print('Not a valid JSON')
+    n = 0
+    for i in r.json():
+        if n < 10:
+            print("{}: {}".format(i.get("sha"),
+                  i.get("commit").get("author").get("name")))
+        n += 1
