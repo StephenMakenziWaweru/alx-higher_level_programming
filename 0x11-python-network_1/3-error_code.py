ef6fd4a42ed9d807410aa3384745b3d762348934
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-09 12:05:16 +0300

    HTTPError

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,11 +4,11 @@ from urllib.request import Request, urlopen
 from urllib.error import HTTPError
 from sys import argv
 
-
-req = Request(argv[1])
-try:
-    with urlopen(req) as response:
-        r = response.read()
-        print(r.decode('utf-8'))
-except HTTPError as e:
-    print('Error code:', e.code)
+if __name__ == '__main__':
+    req = Request(argv[1])
+    try:
+        with urlopen(req) as response:
+            r = response.read()
+            print(r.decode('utf-8'))
+    except HTTPError as e:
+        print('Error code:', e.code)
