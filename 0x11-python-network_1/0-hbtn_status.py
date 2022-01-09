a6d05225b1c7c07e2997d8bf5f407ef9201a3fff
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-09 10:04:02 +0300

    used "

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.request
 
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
     html = response.read()
-    print('Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'
-          .format(type(html), html, html.decode('utf-8')))
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+          .format(type(html), html, html.decode("utf-8")))
