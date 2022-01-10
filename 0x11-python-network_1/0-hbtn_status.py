834add2aa57ec05d94658d46e8e7b37f7d99bfef
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-10 14:20:05 +0300

    added indent

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,10 @@
 from urllib import request
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     with request.urlopen("https://intranet.hbtn.io/status") as response:
         r = response.read()
         p = "Body response:\n\t- type: {}\n\t- content: {}"
         p += "\n\t- utf8 content: {}"
-        print(p.format(type(r), r, r.decode('utf-8')))
+        print(p.format(type(r), r, r.decode("utf-8")))
