f1f523e068227733bd0f658ea05e54982dc611a6
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-10 14:08:30 +0300

    added check for imports

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,8 @@
 """ Fetches https://intranet.hbtn.io/status with urlib"""
 from urllib import request
 
+
+if __name__ == '__main__':
 with request.urlopen("https://intranet.hbtn.io/status") as response:
     r = response.read()
     print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
