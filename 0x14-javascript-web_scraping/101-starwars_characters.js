5ac4db48e8e59c04c36727162d6a9ec379a398f8
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-02-02 14:47:30 +0300

    declaed characters

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 // prints all characters of a Star Wars movie
 const request = require('request');
-const request2 = require('request');
+let characters;
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, (error, response, body) => {
   if (error) throw new Error(error);
-  const characters = JSON.parse(body).characters;
+  characters = JSON.parse(body).characters;
   characters.forEach(character => {
-    request2(character, (error, response, body) => {
+    request(character, (error, response, body) => {
       console.log(JSON.parse(body).name);
     });
   });
