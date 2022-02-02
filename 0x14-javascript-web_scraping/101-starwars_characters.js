e77315516cfd5963eda1ec8d7ddfc676c21995b8
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-02-02 14:41:22 +0300

    101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,10 +5,9 @@ const request2 = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, (error, response, body) => {
   if (error) throw new Error(error);
-  characters = JSON.parse(body).characters;
+  const characters = JSON.parse(body).characters;
   characters.forEach(character => {
     request2(character, (error, response, body) => {
-      if (error) throw new Error(error);
       console.log(JSON.parse(body).name);
     });
   });
