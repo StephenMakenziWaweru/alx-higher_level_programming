3bfbd7a532beb9cbe7a05fc861f3cbd1ba0b576a
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-02-02 12:18:24 +0300

    semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,7 @@ request(process.argv[2], function (error, response, body) {
   const films = JSON.parse(body).results;
   let count = 0;
   films.forEach(film => {
-    const res = film.characters.includes('https://swapi-api.hbtn.io/api/people/18/');
+    const res = film.characters.includes("https://swapi-api.hbtn.io/api/people/18/");
     count += (res ? 1 : 0);
   });
   console.log(count);
