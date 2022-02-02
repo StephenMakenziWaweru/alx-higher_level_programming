d13554165dd49a4d52608ed5b47ddd4760b7d159
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-02-02 12:00:32 +0300

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// prints the number of movies where "Wedge Antilles' is present
+const request = require('request');
+
+request(process.argv[2], function (body) {
+  const films = JSON.parse(body).results;
+  let count = 0;
+  films.forEach(film => {
+    const res = film.characters.includes('https://swapi-api.hbtn.io/api/people/18/');
+    if (res) {
+      count++;
+    }
+  });
+  console.log(count);
+});
